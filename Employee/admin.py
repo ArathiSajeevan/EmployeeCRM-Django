2c9cf2863ed6c372e5d8d43ad26d38b0136d80d6
Arathi Sajeevan <arathisajeeva123@gmail.com>
2023-04-30 19:12:16 +0530

    first commit

---
+++ Employee/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Employee.models import Datas
+
+# Register your models here.
+
+admin.site.register(Datas)
---
+++ Employee/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Datas(models.Model):
+    Emp_no = models.IntegerField(default="")
+    Name = models.CharField(max_length=20,default="")
+    Address = models.CharField(max_length=50,default="")
+    Emp_start_date = models.DateField(max_length=20,default="")
+    Emp_end_date = models.DateField(max_length=20,default="")
+    Image = models.ImageField(default="")
+    Status= models.CharField(max_length=20,default="")
+    
